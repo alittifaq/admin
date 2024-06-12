8414d8d3f61ab1aac5141cd48abf135881bf3dc0
Rolly Maulana Awangga <awangga@gmail.com>
2024-06-13 06:33:48 +0700

    gabung produk dengan upload

---
+++ index.js
@@ -0,0 +1,30 @@
+import {
+  setInner,
+  show,
+  hide,
+  setValue,
+  getFileSize,
+} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.0.6/croot.js";
+import { postFile } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.2/croot.js";
+
+window.uploadImage = uploadImage;
+
+const target_url =
+  "https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/upload/img";
+
+function uploadImage() {
+  if (!getValue("imageInput")) {
+    alert("Please select an image file");
+    return;
+  }
+  hide("inputfile");
+  let besar = getFileSize("imageInput");
+  setInner("isi", besar);
+  postFile(target_url, "imageInput", "image", renderToHtml);
+}
+
+function renderToHtml(result) {
+  console.log(result);
+  setValue("foto", "https://cdn.blkkalittifaq.id/"+result.response);
+  show("inputfile");
+}

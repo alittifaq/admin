736a83301f9b4f22e61eadf59710cf022993b667
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-06-17 13:19:22 +0700

    mengembalikan func semula

--- index.js
+++ index.js
@@ -13,25 +13,19 @@ window.uploadImage = uploadImage;
 const target_url =
   "https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/upload/img";
 
-  function uploadImage() {
-    if (!getValue("imageInput")) {
-      alert("Please select an image file");
-      return;
-    }
-    hide("inputfile");
-    let besar = getFileSize("imageInput");
-    console.log(besar);
-    postFile(target_url, "imageInput", "image", renderToHtml); // Tidak ada .catch di sini
+function uploadImage() {
+  if (!getValue("imageInput")) {
+    alert("Please select an image file");
+    return;
   }
-  
-  function renderToHtml(result) {
-    if (result && result.response) {
-      setValue("foto", "https://cdn.blkkalittifaq.id/" + result.response);
-    } else {
-      console.error("Invalid or empty response:", result);
-      // Handle the error or provide fallback behavior
-    }
-    show("inputfile");
-  }
-  
+  hide("inputfile");
+  let besar = getFileSize("imageInput");
+  console.log(besar);
+  postFile(target_url, "imageInput", "image", renderToHtml);
+}
 
+function renderToHtml(result) {
+  console.log(result);
+  setValue("foto", "https://cdn.blkkalittifaq.id/"+result.response);
+  show("inputfile");
+}

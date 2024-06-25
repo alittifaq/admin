06dfb3e6c1b200ecc79f0bc0bebdd723ace578f5
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-06-25 09:46:33 +0700

    tambah funsi addgalleryitem di dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,14 +1,14 @@
-document.getElementById('products-tab').addEventListener('click', function() {
-    loadProducts();
+document.getElementById("products-tab").addEventListener("click", function () {
+  loadProducts();
 });
 
-document.getElementById('gallery-tab').addEventListener('click', function() {
-    loadGallery();
+document.getElementById("gallery-tab").addEventListener("click", function () {
+  loadGallery();
 });
 
 async function loadProducts() {
-    const content = document.getElementById('content');
-    content.innerHTML = `
+  const content = document.getElementById("content");
+  content.innerHTML = `
         <h2>Products</h2>
         <div class="table-container">
             <table>
@@ -27,14 +27,16 @@ async function loadProducts() {
         <button onclick="addProduct()">Add Product</button>
     `;
 
-    try {
-        const response = await fetch('https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/product');
-        const products = await response.json();
-        const productTableBody = document.getElementById('product-table-body');
-        productTableBody.innerHTML = ''; // Clear the table body before adding new rows
-        products.forEach((product) => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
+  try {
+    const response = await fetch(
+      "https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/product"
+    );
+    const products = await response.json();
+    const productTableBody = document.getElementById("product-table-body");
+    productTableBody.innerHTML = ""; // Clear the table body before adding new rows
+    products.forEach((product) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
                 <td><img src="${product.foto}" alt="${product.nama}" width="50"></td>
                 <td>${product.nama}</td>
                 <td>
@@ -44,17 +46,16 @@ async function loadProducts() {
                     </div>
                 </td>
             `;
-            productTableBody.appendChild(row);
-        });
-    } catch (error) {
-        console.error('Error loading products:', error);
-    }
+      productTableBody.appendChild(row);
+    });
+  } catch (error) {
+    console.error("Error loading products:", error);
+  }
 }
 
-
 async function loadGallery() {
-    const content = document.getElementById('content');
-    content.innerHTML = `
+  const content = document.getElementById("content");
+  content.innerHTML = `
         <h2>Gallery</h2>
         <div class="table-container">
             <table>
@@ -74,14 +75,16 @@ async function loadGallery() {
         <button onclick="addGalleryItem()">Add Gallery Item</button>
     `;
 
-    try {
-        const response = await fetch('https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/gallery');
-        const gallery = await response.json();
-        const galleryTableBody = document.getElementById('gallery-table-body');
-        galleryTableBody.innerHTML = ''; // Clear the table body before adding new rows
-        gallery.forEach((item) => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
+  try {
+    const response = await fetch(
+      "https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/gallery"
+    );
+    const gallery = await response.json();
+    const galleryTableBody = document.getElementById("gallery-table-body");
+    galleryTableBody.innerHTML = ""; // Clear the table body before adding new rows
+    gallery.forEach((item) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
                 <td><img src="${item.foto}" alt="${item.judul_kegiatan}" width="50"></td>
                 <td>${item.judul_kegiatan}</td>
                 <td>${item.tahun}</td>
@@ -92,94 +95,98 @@ async function loadGallery() {
                     </div>
                 </td>
             `;
-            galleryTableBody.appendChild(row);
-        });
-    } catch (error) {
-        console.error('Error loading gallery:', error);
-    }
+      galleryTableBody.appendChild(row);
+    });
+  } catch (error) {
+    console.error("Error loading gallery:", error);
+  }
 }
 
-
 function logout() {
-    window.location.href = 'https://www.blkkalittifaq.id/dashboard/login.html';
+  window.location.href = "https://www.blkkalittifaq.id/dashboard/login.html";
 }
 
 async function addProduct() {
-    window.location.href = 'productform.html';
+  window.location.href = "productform.html";
 }
 
 async function editProduct() {
-    var editName = prompt("Enter the Product Name to edit:");
-  
-    if (editName) {
-      var editData = {
-        foto: document.getElementById("foto").value,
-        nama: document.getElementById("nama").value,
-        name: editName,
-      };
-  
-      fetch(
-        `https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/product`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(editData),
-        }
-      )
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          return response.text(); // Use text() to get raw response
-        })
-        .then((text) => {
-          try {
-            const data = JSON.parse(text); // Try to parse JSON
-            console.log("Edit Success:", data);
-          } catch (error) {
-            console.error("Error parsing JSON:", error);
-            console.log("Raw response:", text); // Log raw response for debugging
-          }
-        })
-        .catch((error) => {
-          console.error("Fetch error:", error);
-        });
-    }
-  }
-  
-  
-async function deleteGalleryItem(galleryTitle) {
-    try {
-        const deleteData = {
-            judul_kegiatan: galleryTitle.toString(), // Convert galleryTitle to string
-        };
+  var editName = prompt("Enter the Product Name to edit:");
 
-        const response = await fetch(`https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/gallery`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(deleteData),
-        });
+  if (editName) {
+    var editData = {
+      foto: document.getElementById("foto").value,
+      nama: document.getElementById("nama").value,
+      name: editName,
+    };
 
+    fetch(
+      `https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/product`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(editData),
+      }
+    )
+      .then((response) => {
         if (!response.ok) {
-            throw new Error("Network response was not ok");
+          throw new Error("Network response was not ok");
         }
-
-        const text = await response.text();
+        return response.text(); // Use text() to get raw response
+      })
+      .then((text) => {
         try {
-            const data = JSON.parse(text);
-            console.log("Delete Success:", data);
-            // Refresh the gallery list
-            loadGallery(); // Call loadGallery to refresh the list
+          const data = JSON.parse(text); // Try to parse JSON
+          console.log("Edit Success:", data);
         } catch (error) {
-            console.error("Error parsing JSON:", error);
-            console.log("Raw response:", text);
+          console.error("Error parsing JSON:", error);
+          console.log("Raw response:", text); // Log raw response for debugging
         }
-    } catch (error) {
+      })
+      .catch((error) => {
         console.error("Fetch error:", error);
-    }
+      });
+  }
 }
 
+async function addGalleryItem() {
+  window.location.href = "galleryform.html";
+}
+
+async function deleteGalleryItem(galleryTitle) {
+  try {
+    const deleteData = {
+      judul_kegiatan: galleryTitle.toString(), // Convert galleryTitle to string
+    };
+
+    const response = await fetch(
+      `https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/gallery`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(deleteData),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const text = await response.text();
+    try {
+      const data = JSON.parse(text);
+      console.log("Delete Success:", data);
+      // Refresh the gallery list
+      loadGallery(); // Call loadGallery to refresh the list
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+      console.log("Raw response:", text);
+    }
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
+}

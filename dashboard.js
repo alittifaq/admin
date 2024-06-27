eeb79aa20a58cd2465fc84584ef38cbd66bb35e0
fahiraaa06 <fahira1678@gmail.com>
2024-06-27 12:08:10 +0700

    modified dashboard js

--- dashboard.js
+++ dashboard.js
@@ -6,6 +6,12 @@ document.getElementById("gallery-tab").addEventListener("click", function () {
   loadGallery();
 });
 
+function logout() {
+  window.location.href = "https://www.blkkalittifaq.id/dashboard/login.html";
+}
+
+
+//product
 async function loadProducts() {
   const content = document.getElementById("content");
   content.innerHTML = `
@@ -53,58 +59,7 @@ async function loadProducts() {
   }
 }
 
-async function loadGallery() {
-  const content = document.getElementById("content");
-  content.innerHTML = `
-        <h2>Gallery</h2>
-        <div class="table-container">
-            <table>
-                <thead>
-                    <tr>
-                        <th>Foto</th>
-                        <th>Judul Kegiatan</th>
-                        <th>Tahun Kegiatan</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody id="gallery-table-body">
-                    <!-- Galeri akan dimuat di sini -->
-                </tbody>
-            </table>
-        </div>
-        <button onclick="addGalleryItem()">Add Gallery Item</button>
-    `;
 
-  try {
-    const response = await fetch(
-      "https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/gallery"
-    );
-    const gallery = await response.json();
-    const galleryTableBody = document.getElementById("gallery-table-body");
-    galleryTableBody.innerHTML = ""; // Clear the table body before adding new rows
-    gallery.forEach((item) => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-                <td><img src="${item.foto}" alt="${item.judul_kegiatan}" width="50"></td>
-                <td>${item.judul_kegiatan}</td>
-                <td>${item.tahun}</td>
-                <td>
-                    <div class="action-buttons">
-                        <button class="edit" onclick="editGalleryItem('${item.judul_kegiatan}')">Edit</button>
-                        <button class="delete" onclick="deleteGalleryItem('${item.judul_kegiatan}')">Delete</button>
-                    </div>
-                </td>
-            `;
-      galleryTableBody.appendChild(row);
-    });
-  } catch (error) {
-    console.error("Error loading gallery:", error);
-  }
-}
-
-function logout() {
-  window.location.href = "https://www.blkkalittifaq.id/dashboard/login.html";
-}
 
 async function addProduct() {
   window.location.href = "productform.html";
@@ -205,6 +160,56 @@ async function deleteProduct(productTitle) {
   } catch (error) {
     console.error("Fetch error:", error);
     alert("Gagal hapus produk, coba lagi ya!");
+  }
+}
+
+//gallery
+async function loadGallery() {
+  const content = document.getElementById("content");
+  content.innerHTML = `
+        <h2>Gallery</h2>
+        <div class="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Foto</th>
+                        <th>Judul Kegiatan</th>
+                        <th>Tahun Kegiatan</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody id="gallery-table-body">
+                    <!-- Galeri akan dimuat di sini -->
+                </tbody>
+            </table>
+        </div>
+        <button onclick="addGalleryItem()">Add Gallery Item</button>
+    `;
+
+  try {
+    const response = await fetch(
+      "https://asia-southeast2-blkkalittifaq-426014.cloudfunctions.net/blkkalittifaq/data/gallery"
+    );
+    const gallery = await response.json();
+    const galleryTableBody = document.getElementById("gallery-table-body");
+    galleryTableBody.innerHTML = ""; // Clear the table body before adding new rows
+    gallery.forEach((item) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `
+                <td><img src="${item.foto}" alt="${item.judul_kegiatan}" width="50"></td>
+                <td>${item.judul_kegiatan}</td>
+                <td>${item.tahun}</td>
+                <td>
+                    <div class="action-buttons">
+                        <button class="edit" onclick="editGalleryItem('${item.judul_kegiatan}')">Edit</button>
+                        <button class="delete" onclick="deleteGalleryItem('${item.judul_kegiatan}')">Delete</button>
+                    </div>
+                </td>
+            `;
+      galleryTableBody.appendChild(row);
+    });
+  } catch (error) {
+    console.error("Error loading gallery:", error);
   }
 }
 
